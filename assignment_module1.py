2ebd2dc2692e42447a61cb19dd923c2403ad1584
MuhammadRaheel75 <90866963+MuhammadRaheel75@users.noreply.github.com>
2021-09-21 13:16:09 -0600

    Add files via upload

---
+++ assignment_module1.py
@@ -0,0 +1,36 @@
+"""
+Assignment_Module1
+"""
+a = """
+name   = John Cena 
+Age    = 54
+Address= error-you cant see him
+phone #= 780-439-6942
+"""
+
+b = """
+name   = The Rock
+age    = 49
+Address= Eastglen Highschool
+phone #= 587-123-4567
+"""
+
+c = """
+name   = Vin Diesel
+age    = Infinity
+Address= Family Centre
+phone #= 780-464-6243
+"""
+d = """
+name   = Ludacris
+age    = 44
+Address= NASA Headquarters
+phone #= 911
+"""
+
+e = """
+name   = Charlize Theron
+age    = 21
+Address= Your local superstore
+phone  = 420-987-6543
+"""
