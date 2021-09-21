5944c8fbca7a800bbad701410073f0c5e94ef015
MuhammadRaheel75 <90866963+MuhammadRaheel75@users.noreply.github.com>
2021-09-21 13:16:23 -0600

    Add files via upload

---
+++ TryingToDoTheAssignment.py
@@ -0,0 +1,109 @@
+# Importing Module and the info from other file
+
+import assignment_module1
+
+aa = assignment_module1.a
+bb = assignment_module1.b
+cc = assignment_module1.c
+dd = assignment_module1.d
+ee = assignment_module1.e
+
+# Intro to phonebook
+
+name = input ("What is your name ")
+print ("Nice to meet you", name)
+
+print ("Welcome to the fast and the furious headquarters.")
+print ("This is a newly developed program for you to contact the Actors.")
+print (" You can contact, John Cena, The Rock, Vin Diesel, Ludacris, or Charlize Theron")
+# Input/Finding out Contact Code
+
+print (" Have fun!!!")
+choice = (input("which actor would you like to talk to? "))
+
+if choice == "John Cena":
+    print ("Here you go:", aa)
+
+elif choice == "The Rock":
+    print ("Here you go", bb)
+
+elif choice == "Vin Diesel":
+    print ("Here you go", cc)
+
+elif choice == "Ludacris":
+    print ("Here you go", dd)
+
+elif choice == "Charlize Theron":
+    print ("Here you go", ee)
+
+else :
+    print ("That choice does not exist, exit the program or try again with exact way as listed")
+
+#TRY NUMBER 2
+
+choice = (input("which actor would you like to talk to next? if not close the program "))
+
+if choice == "John Cena":
+    print ("Here you go:", aa)
+
+elif choice == "The Rock":
+    print ("Here you go", bb)
+
+elif choice == "Vin Diesel":
+    print ("Here you go", cc)
+
+elif choice == "Ludacris":
+    print ("Here you go", dd)
+
+elif choice == "Charlize Theron":
+    print ("Here you go", ee)
+
+else :
+    print ("That choice does not exist, exit the program or try again with exact way as listed, 2 MORE REMAINING")
+
+#TRY NUMBER 3
+
+choice = (input("which actor would you like to talk to next? if not close the program "))
+
+if choice == "John Cena":
+    print ("Here you go:", aa)
+
+elif choice == "The Rock":
+    print ("Here you go", bb)
+
+elif choice == "Vin Diesel":
+    print ("Here you go", cc)
+
+elif choice == "Ludacris":
+    print ("Here you go", dd)
+
+elif choice == "Charlize Theron":
+    print ("Here you go", ee)
+
+else :
+    print ("That choice does not exist, exit the program or try again with exact way as listed, 1 MORE TRY REMAINING")
+
+# LAST TRY
+
+choice = (input("which actor would you like to talk to next? if not close the program "))
+
+if choice == "John Cena":
+    print ("Here you go:", aa)
+
+elif choice == "The Rock":
+    print ("Here you go", bb)
+
+elif choice == "Vin Diesel":
+    print ("Here you go", cc)
+
+elif choice == "Ludacris":
+    print ("Here you go", dd)
+
+elif choice == "Charlize Theron":
+    print ("Here you go", ee)
+
+else :
+    print ("That choice does not exist, Please close the program")
+
+
+
